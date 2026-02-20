841818091f6c7d77b27c81177817a83c02c06503
Christopher Revell <christopher.revell@manchester.ac.uk>
2026-02-20 14:34:42 +0000

    Tidying

--- src/TopologyFunctions.jl
+++ src/TopologyFunctions.jl
@@ -45,36 +45,12 @@ function findNormalEdges(A, B)
     return jⁿ
 end
 function findÂ(A, B)
-    # J = size(A, 1)
-    # K = size(A, 2)
-    # jⁿ = findNormalEdges(A,B)
-    # jᵖ = findPeripheralEdges(B)
-    # jⁱ = 1 .-jⁿ.-jᵖ
-    # kᵖ = findPeripheralVertices(A,B)
-    # kⁱ = 1 .-kᵖ
-    # Aⁿⁱ = A[jⁿ.==1, kⁱ.==1]
-    # Aⁿⁱᵀ = Transpose(Aⁿⁱ)
-    # Aⁱⁱ = A[jⁱ.==1, kⁱ.==1]
-    # Â = spzeros(Int64, sum(jⁿ)+sum(jⁱ), sum(kⁱ))
-    # Â[1:sum(jⁿ),:] .= Aⁿⁱ
-    # Â[sum(jⁿ)+1:end,:] .= Aⁱⁱ
-    # return Â
     jᵖ = findPeripheralEdges(B)
     kᵖ = findPeripheralVertices(A,B)
     Â = A[jᵖ.==0, kᵖ.==0]
     return Â
 end
 function findB̂(A, B)
-    # J = size(A, 1)
-    # jⁿ = findNormalEdges(A,B)
-    # jᵖ = findPeripheralEdges(B)
-    # jⁱ = 1 .-jⁿ.-jᵖ
-    # Bⁿ = B[:,jⁿ.==1]
-    # Bⁱ = B[:, jⁱ.==1]
-    # B̂ = spzeros(Int64, size(B,1), sum(jⁱ)+sum(jⁿ))
-    # B̂[:,1:sum(jⁿ)] .= Bⁿ
-    # B̂[:,sum(jⁿ)+1:end] .= Bⁱ
-    # return B̂
     jᵖ = findPeripheralEdges(B)
     B̂ = B[:, jᵖ.==0]
     return B̂
@@ -110,36 +86,12 @@ function findNormalEdges!(A, B, jⁿ)
     return nothing
 end
 function findÂ!(A, B, Â)
-    # J = size(A, 1)
-    # K = size(A, 2)
-    # jⁿ = findNormalEdges(A,B)
-    # jᵖ = findPeripheralEdges(B)
-    # jⁱ = 1 .-jⁿ.-jᵖ
-    # kᵖ = findPeripheralVertices(A,B)
-    # kⁱ = 1 .-kᵖ
-    # Aⁿⁱ = A[jⁿ.==1, kⁱ.==1]
-    # Aⁿⁱᵀ = Transpose(Aⁿⁱ)
-    # Aⁱⁱ = A[jⁱ.==1, kⁱ.==1]
-    # Â = spzeros(Int64, sum(jⁿ)+sum(jⁱ), sum(kⁱ))
-    # Â[1:sum(jⁿ),:] .= Aⁿⁱ
-    # Â[sum(jⁿ)+1:end,:] .= Aⁱⁱ
-    # return Â
     jᵖ = findPeripheralEdges(B)
     kᵖ = findPeripheralVertices(A,B)
     Â .= A[jᵖ.==0, kᵖ.==0]
     return Â
 end
 function findB̂!(A, B, B̂)
-    # J = size(A, 1)
-    # jⁿ = findNormalEdges(A,B)
-    # jᵖ = findPeripheralEdges(B)
-    # jⁱ = 1 .-jⁿ.-jᵖ
-    # Bⁿ = B[:,jⁿ.==1]
-    # Bⁱ = B[:, jⁱ.==1]
-    # B̂ = spzeros(Int64, size(B,1), sum(jⁱ)+sum(jⁿ))
-    # B̂[:,1:sum(jⁿ)] .= Bⁿ
-    # B̂[:,sum(jⁿ)+1:end] .= Bⁱ
-    # return B̂
     jᵖ = findPeripheralEdges(B)
     B̂ .= B[:, jᵖ.==0]
     return B̂
@@ -189,41 +141,3 @@ export findNormalEdges!
 export senseCheck
 
 end
-
-
-# function topologyMatrices(A, B)
-
-#     # Find adjacency matrices from incidence matrices
-#     Ā = abs.(A)    # All -1 components converted to +1 (In other words, create adjacency matrix Ā from incidence matrix A)Ā :==>= a:C=>bs.:(A=>(A) :  =>   #: A=> All: =>: 1 :components co=>components conv:erted to +1 (In =>erted to +1 (In ot:her words, create adjacency matrix Ā from incidence matrix A=>her words, create adjacency matrix Ā from incidence matrix A)
-#     B̄ = abs.(B)    # All -1 components converted to +1 (In other words, create adjacency matrix B̄ from incidence matrix B)
-#     # C adjacency matrix. Rows => cells; Columns => vertices.
-#     C = B̄ * Ā .÷ 2 # (NB Integer division)
-#     dropzeros!(C)
-#     # Update transpose matrices
-#     Aᵀ = Transpose(A)
-#     Āᵀ = abs.(Aᵀ)
-#     Bᵀ = Transpose(B)
-#     B̄ᵀ = abs.(Bᵀ)
-
-  
-#     # Calculate additional topology data
-#     # Number of edges around each cell found by summing columns of B̄
-#     cellEdgeCount = sum.(eachrow(B̄))
-
-#     # Find boundary vertices
-#     # Summing each column of B finds boundary edges (for all other edges, cell orientations on either side cancel);
-#     # multiplying by Aᵀ gives nonzero values only where a vertex (row) has nonzero values at columns (edges) corresponding to nonzero values in the list of boundary edges.
-#     # Note that the abs is needed in case the direction of boundary edges cancel at a vertex
-#     boundaryVertices = Āᵀ * abs.(sum.(eachcol(B))) .÷ 2
-
-#     # Find list of edges at system periphery
-#     peripheralEdges = abs.([sum(x) for x in eachcol(B)])
-
-#     cellVertexOrders  = fill(CircularVector(Int64[]), size(B, 1))
-#     cellEdgeOrders    = fill(CircularVector(Int64[]), size(B, 1))
-#     for i = 1:size(B,1)
-#         cellVertexOrders[i], cellEdgeOrders[i] = orderAroundCell(matrices, i)
-#     end
-
-#     return Dict(:Ā=>Ā, :B̄=>B̄, :C=>C, :Aᵀ=>Aᵀ, :Āᵀ=>Āᵀ, :Bᵀ=>Bᵀ, :B̄ᵀ=>B̄ᵀ, :cellEdgeCount=>cellEdgeCount, :boundaryVertices=>boundaryVertices, :peripheralEdges=>peripheralEdges, :cellVertexOrders=>cellVertexOrders, :cellEdgeOrders=>cellEdgeOrders)
-# end
